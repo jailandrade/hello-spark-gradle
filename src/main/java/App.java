ffb666fc8118f52f2d5962ef8b41f83a1ddda3a0
Mikhail Cruz Andrade <jailandrade@gmail.com>
2017-11-17 20:38:45 -0600

    use of images and public resources

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,11 @@
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {
-    get("/hello", (request, response) -> "Hello world");
+	public static void main(String[] args) {
+	
+	staticFileLocation("/public/"); 
+	
+	get("/hello", (request, response) -> "Hello world");
 
 	get("/letter", (request, response) -> 
 			"<!DOCTYPE html>" +
@@ -18,6 +21,7 @@ public class App {
 			"<p>This was not an easy decision to make. The past ten years have been very rewarding. I've enjoyed working for you and managing a very successful team dedicated to a quality product delivered on time.</p>" + 
 			"<p>Thank you for the opportunities for growth that you have provided me.</p>" + 
 			"<p>I wish you and the company all the best. If I can be of any help during the transition, please don't hesitate to ask.</p>" + 
+			"<p><img src='/images/firma.png' alt='signature'></p>" + 
 			"</div>" +
 			"</body>" +
 			"</html>"
